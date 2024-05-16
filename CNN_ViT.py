260eb6970a82856211e5af66ebc02236d58a4a2a
Yulian <72699313+yulian-lyubomirov@users.noreply.github.com>
2024-05-16 09:52:37 +0200

    Update CNN_ViT.py
    Add LayerNorm

--- CNN_ViT.py
+++ CNN_ViT.py
@@ -40,6 +40,7 @@ class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout = 0.):
         super().__init__()
         self.net = nn.Sequential(
+            nn.LayerNorm(dim),
             nn.Linear(dim, hidden_dim),
             nn.GELU(),
             nn.Dropout(dropout),
